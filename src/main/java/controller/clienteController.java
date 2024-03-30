9743679fecce2b34c71e416deb0e6c5c94d81251
Yan Falcão <56008873+YanBFalcao@users.noreply.github.com>
2024-03-30 17:15:58 -0300

    Commit Update - 30/03/2024
    Commit Update - 30/03/2024

--- src/main/java/controller/clienteController.java
+++ src/main/java/controller/clienteController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import com.sun.tools.javac.Main;
+import model.DTO.ClienteResponseDTO;
 import model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,23 +14,24 @@ import java.util.logging.Logger;
 public class clienteController {
 
     @Autowired
-    private ClienteRepository clienteRepositoryc; // Instanciando objeto para passar dados para Interfaces
+    private ClienteRepository clienteRepository; // Instanciando objeto para passar dados para Interfaces
 
     // ====================== || ======================
     //                  GET ALL CLIENTES
     // ====================== || ======================
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping // ENDPOIT (GET) - CONSULTAR TODOS OS ITENS
-    public List<FoodResponseDTO> getAll(){
-        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+    public List<ClienteResponseDTO> getAll(){
+        List<ClienteResponseDTO> clienteList = clienteRepository.findAll().stream().map(ClienteResponseDTO::new).toList();
         try {
-            return foodList;
+            return clienteList;
         }
         catch(Exception e){
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Ocorreu um erro: ", e);
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "\nOcorreu um erro: ", e + "\n");
         }
-        return foodList;
+        return clienteList;
     }
+
 
 
 
